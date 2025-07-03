cc4111df1dc83b25105ae942ec3af83a03fae1f1
Narendra Pal <narenderpal.singhnsr@gmail.com>
2025-07-04 00:53:56 +0530

    slash cors issue resolving

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://skim-alpha.vercel.app/', // frontend port
+    origin: 'https://skim-alpha.vercel.app', // frontend port
     credentials: true,
   });
   await app.listen(process.env.DB_PORT ?? 3001);
